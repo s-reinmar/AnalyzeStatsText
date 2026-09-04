4b00c883cf7ebfcd314cdba989da320b5f5aa1d8
cohenm <slawek.rebis@gmail.com>
2026-09-04 10:56:58 +0200

    commit 0.3 - Wydzielenie metod do osobnego pliku (Refactor text processing methods and move to TextMethod class)

--- src/main/java/pl/j/reinmar/app/TextApp.java
+++ src/main/java/pl/j/reinmar/app/TextApp.java
@@ -1,55 +1,8 @@
-package com.cohenm.analyzer.app;
+package pl.j.reinmar.app;
 
-import java.util.ArrayList;
+import static pl.j.reinmar.app.TextMethod.*;
 
 public class TextApp {
-
-    /**
-     * countWords -> dla tekstu wykonuje zamianę wszystkich znaków interpunkcyjnych na spacje
-     * oraz usuwa spację
-     * @param text
-     * @return ilość słów
-     */
-    private static int countWords(String text) {
-        if (text == null) return 0;
-        String trimmed = text.trim();
-        if (trimmed.isEmpty()) return 0;
-
-        trimmed = trimmed.replaceAll("[\\p{Punct}„”»«]", " ");
-        String[] parts = trimmed.split("\\s+");
-
-        // przenoszenie do ArrayList
-        ArrayList<String> words = new ArrayList<String>();
-        for (String part : parts) {
-            if(!part.isBlank()) {
-                words.add(part);
-            }
-        }
-        return words.size();
-    }
-
-    /**
-     * Liczy znaki ze spacjami
-     * @param text
-     * @return ilość znaków ze spacjami
-     */
-    private static int countCharsWithSpaces(String text) {
-        if(text == null) {
-            return 0;
-        }
-        return text.length();
-    }
-
-    /**
-     * Liczy znaki bez spacji
-     * @param text
-     * @return ilość znaków bez spacji
-     */
-    private static int countCharsWithoutSpaces(String text) {
-        if (text == null) return 0;
-        String withoutWhiteSpace = text.replaceAll("\\s+","");
-        return withoutWhiteSpace.length();
-    }
 
     public static void main(String[] args) {
         // Przykładowy tekst — później możesz podawać go z args lub Scannerem
